9280a65316032c77782315aca791fa24a723d07e
kellydeclercq <204192948+kellydeclercq@users.noreply.github.com>
2025-11-12 12:31:23 +0100

    UI WPF

---
+++ OrderProcessingMembersUI_2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OrderProcessingMembersBL.Enums;
+using OrderProcessingMembersBL.Manager;
+using OrderProcessingMembersBL.Models;
+using OrderProcessingMembersUI_2;
+
+namespace MainOrderProcessing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private OrderProcessingMembersBeheerder _manager;
+        public MainWindow(OrderProcessingMembersBeheerder manager)
+        {
+            InitializeComponent();  
+            _manager = manager;
+        }
+
+        private void Click_LoginMember(object sender, RoutedEventArgs e)
+        {
+            string email = TextBox_Email.Text;
+            Member memberLo = _manager.GetMemberByEmail(email);
+
+            EventOrderWindow w = new EventOrderWindow(memberLo, _manager);
+
+            if (RadioButton_Admin.IsChecked == true)
+            {
+                //TODO add window admin with list of orders and their benefits
+                AdminWindow adminw = new AdminWindow();
+                adminw.ShowDialog();
+            }
+            else
+            {
+                w.ShowDialog();
+            }
+          
+
+        }
+    }
+}
