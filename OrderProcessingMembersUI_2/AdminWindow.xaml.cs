dbe9a24be79be881d7d05cc304832dcd18d9285f
kellydeclercq <204192948+kellydeclercq@users.noreply.github.com>
2025-11-12 19:10:12 +0100

    UI update adminwindow

--- OrderProcessingMembersUI_2/AdminWindow.xaml.cs
+++ OrderProcessingMembersUI_2/AdminWindow.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using OrderProcessingMembersBL.Enums;
+using OrderProcessingMembersBL.Models;
+using OrderProcessingMembersBL.Models.Calculators;
+using OrderProcessingMembersBL.Models.Deliveries;
+using OrderProcessingMembersBL.Models.Status;
 
 namespace OrderProcessingMembersUI_2
 {
@@ -19,9 +24,43 @@ namespace OrderProcessingMembersUI_2
     /// </summary>
     public partial class AdminWindow : Window
     {
+
+        Dictionary<Event, List<StandardOrder>> benefits = new Dictionary<Event, List<StandardOrder>>();
+        List<StandardOrder> orders = new List<StandardOrder>();
+
         public AdminWindow()
         {
             InitializeComponent();
+
+            Address address = new Address("Werchter", "Straat", "25", 2000);
+            Address address1 = new Address("Gent", "Straat", "12", 9000);
+            Event ev = new Event("Rock Werchter", new DateTime(26, 07, 14), 140.50, address);
+            Member member = new Member("Lucas", "Email", EStatus.Gold, address1);
+            List<string> benefits = new List<string>() { "nametag", "taxi", "dinner"};
+            
+
+            StandardOrder order = new GoldOrder(ev, member, 5, new ExpressDelivery(), new GoldCalculator());
+            orders.Add(order);
+
+            benefits.Add(ev, orders);
+
+
+
+
+            var x = benefits.SelectMany(x => x.Value).Select(x => new
+            {
+                Eventname = x.Event.Name,
+                EventAdress = x.Event.Address,
+                MemberName = x.Member.Name,
+                MemberAdress = x.Member.Address,
+                Benefits = string.Join(',', x.Benefits)
+            });
+           
+                
+            
+
+            DataGrid_Benefits.ItemsSource = x;
+
         }
     }
 }
